4651525df533b7cf3496af629ae152f276c063fd
Sam Garson <samtgarson@gmail.com>
2016-08-30 08:12:06 +0100

    Basic JWT support with Devise for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
+  include AuthTokenHandling
+  before_action :authenticate_request!, except: [:heartbeat]
+
+  def home
+    render json: { lunar_and_flo: 'welcome' }
+  end
+
   def heartbeat
     Rails.logger.info 'Heartbeat: beep boop.'
-    render json: {hearbeat: 'beep boop'}, status: 200
+    render json: { hearbeat: 'beep boop' }, status: 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'application#home'
+  devise_for :users
   get :heartbeat, to: 'application#heartbeat'
+  post 'auth' => 'authentication#authenticate_user'
 
   mount ForestLiana::Engine => '/forest'
+
   constraints subdomain: 'api', defaults: {format: :json} do
     scope module: 'api' do
       namespace :v1 do
